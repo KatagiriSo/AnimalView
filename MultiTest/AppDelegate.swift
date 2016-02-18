39c17cb6ee2543206c9e7b10246b4ae283df4a97
Katagiri <katagiri@plala.co.jp>
2016-02-18 15:51:45 +0900

    copyright

--- MultiTest/AppDelegate.swift
+++ MultiTest/AppDelegate.swift
@@ -3,8 +3,9 @@
 //  MultiTest
 //
 //  Created by 片桐奏羽 on 2016/02/18.
-//  Copyright (c) 2016年 SoKatagiri. All rights reserved.
-//
+//  Copyright (c) 2016年 SoKatagiri. 
+//  This software is released under the MIT License.
+//  http://opensource.org/licenses/mit-license.php//
 
 import UIKit
 
