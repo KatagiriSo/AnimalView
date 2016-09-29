04c5d88b62a0c68a3a13e6e41badbc25a7c14033
KatagiriSo <katagiri@rodhos.info>
2016-09-29 23:28:57 +0900

    compile by State

--- MultiTest/AnimalView.swift
+++ MultiTest/AnimalView.swift
@@ -10,30 +10,6 @@
 import UIKit
 import QuartzCore
 
-func randumAddPoint(point:CGPoint) -> CGPoint {
-    
-    var x = point.x
-    var y = point.y
-    switch (arc4random() % 4) {
-    case 0: x += 1
-    case 1: y += 1
-    case 2: x -= 1
-    case 3: y -= 1
-    default:
-        break
-    }
-    
-    return CGPoint(x:x, y:y);
-}
-
-typealias Vec = CGPoint
-typealias dVec = CGPoint
-typealias Radius = CGFloat
-typealias dRadius = CGFloat
-typealias State = (Vec, dVec, Radius, dRadius)
-typealias LineState = (origin:Vec, speed:dVec, direction:Vec, length:CGFloat, life:CGFloat)
-
-
 extension UITouch {
     func location(v:UIView) -> CGPoint {
         return self.locationInView(v)
@@ -74,15 +50,97 @@ extension CGContext {
     }
 }
 
+func randumAddPoint(point:CGPoint) -> CGPoint {
+    
+    var x = point.x
+    var y = point.y
+    switch (arc4random() % 4) {
+    case 0: x += 1
+    case 1: y += 1
+    case 2: x -= 1
+    case 3: y -= 1
+    default:
+        break
+    }
+    
+    return CGPoint(x:x, y:y);
+}
+
+typealias Vec = CGPoint
+typealias dVec = CGPoint
+typealias Radius = CGFloat
+typealias dRadius = CGFloat
+
+protocol State {
+    var origin:Vec {get}
+    var speed:Vec {get}
+    
+    func draw(v:AnimalView)
+}
+
+enum StateConfig {
+    case line,circle
+    
+    func toggle()->StateConfig
+    {
+        switch self {
+        case .line:
+            return .circle
+        case .circle:
+            return .line
+        }
+    }
+}
+
+struct CircleState : State {
+    let origin:Vec
+    let speed:dVec
+    let radius:Radius
+    let radSpeed:dRadius
+    
+    var tuple:(origin:Vec,speed:dVec,radius:Radius,radSpeed:dRadius) {
+        return (origin:origin, speed:speed, radius:radius, radSpeed:radSpeed)
+    }
+    
+    func draw(v: AnimalView) {
+        v.drawCircle(self.origin, radius: self.radius)
+    }
+}
+
+struct LineState : State {
+    let origin:Vec
+    let speed:dVec
+    let direction:Vec
+    let length:CGFloat
+    let life:CGFloat
+    
+    var tuple:(origin:Vec, speed:dVec, direction:Vec, length:CGFloat, life:CGFloat) {
+        return (origin:origin, speed:speed, direction:direction, length:length, life:life)
+    }
+    
+    func draw(v:AnimalView) {
+        v.drawLine(self.origin, direction: self.direction, length: self.length)
+    }
+}
+
+//typealias RadiusState = (origin:Vec, speed:dVec, radius:Radius, radSpeed:dRadius)
+//typealias LineState = (origin:Vec, speed:dVec, direction:Vec, length:CGFloat, life:CGFloat)
+
+
+
+
+
+
 class AnimalView: UIView {
-    
+
     var touchmode:Bool = false
-    var touchPoint:CGPoint = CGPoint.zero
+    var touchPoint:CGPoint?
     
-    //var context:CGContext?
-    var list:[LineState]? = []
+    var list:[State]? = []
+    var config:StateConfig = .line
     
     var context:CGContext? = nil
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -95,6 +153,22 @@ class AnimalView: UIView {
         start()
     }
     
+    func setup() {
+        switch config {
+        case .circle:
+            list = initialState(100, point: touchPoint, radius:1)
+        case .line:
+            list = initialState(100, point: touchPoint, direction: CGPoint(x:10,y:1), length: 10, life: 5)
+        }
+    }
+    
+    func start() {
+        
+        self.multipleTouchEnabled = true
+        
+        setup()
+        NSTimer.scheduledTimerWithTimeInterval(0.016, target: self, selector: #selector(AnimalView.timeFired(_:)), userInfo: nil, repeats: true)
+    }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         touchmode = true
@@ -102,6 +176,17 @@ class AnimalView: UIView {
             let point = touch.locationInView(self)
             touchPoint = point
         }
+        
+        switch touches.count {
+        case 1:
+            break
+        case 2...5:
+            config = config.toggle()
+            setup()
+        default:
+            break
+        }
+
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -155,55 +240,99 @@ class AnimalView: UIView {
         
     }
     
-    func update(list:[LineState]) -> [LineState] {
+    func update(state:State) -> State {
+        if let state = state as? LineState {
+            return update(state)
+        }
+        
+        if let state = state as? CircleState {
+            return update(state)
+        }
+        return state
+    }
+    
+    func update(state:LineState) -> State {
         
         let width = self.frame.width
         let height = self.frame.height
         
-        return list.map { (state:LineState) -> LineState in
-            
-            let l = state.length
-            let life = state.life
-            var d = state.direction
-    
-            let angle = CGFloat(M_PI_4 * (1/90) * 60)
-            d.x = cos(angle) * d.x - sin(angle) * d.y
-            d.y = sin(angle) * d.x + cos(angle) * d.y
-            let dd = sqrt(d.x*d.x + d.y*d.y)
-            d.x = d.x/dd
-            d.y = d.y/dd
-            
-            var r = state.origin
-            var v = state.speed
-            
-            
-            
-            v = randumAddPoint(v)
-            
-//            if touchmode {
-//                v = touchEffect(state: state)
-//            }
-            
-            r.x = r.x + v.x / 5
-            r.y = r.y + v.y / 5
-            if (r.y > height) {r.y = height;v.y = 0} else if (r.y <= 5)    {r.y = 5; v.y = 0}
-            if (r.x <= 5)     {r.x = 5; v.x = 0}     else if (r.x > width) {r.x = width;v.x = 0}
-            
-            let ret:LineState = LineState(r,v,d,l,life)
-            return ret
+        let l = state.length
+        let life = state.life
+        var d = state.direction
+        
+        let angle = CGFloat(M_PI_4 * (1/90) * 60)
+        d.x = cos(angle) * d.x - sin(angle) * d.y
+        d.y = sin(angle) * d.x + cos(angle) * d.y
+        let dd = sqrt(d.x*d.x + d.y*d.y)
+        d.x = d.x/dd
+        d.y = d.y/dd
+        
+        var r = state.origin
+        var v = state.speed
+        
+        
+        
+        v = randumAddPoint(v)
+        
+        if touchmode {
+            v = touchEffect(state, touchPoint: touchPoint ?? center)
         }
+        
+        r.x = r.x + v.x / 5
+        r.y = r.y + v.y / 5
+        if (r.y > height) {r.y = height;v.y = 0} else if (r.y <= 5)    {r.y = 5; v.y = 0}
+        if (r.x <= 5)     {r.x = 5; v.x = 0}     else if (r.x > width) {r.x = width;v.x = 0}
+        
+        let ret:LineState = LineState(origin: r, speed: v, direction: d, length: l, life: life)
+        return ret
     }
     
-    func touchEffect(state:State) -> CGPoint {
-        var v : CGPoint = state.1
-        if (state.0.x > touchPoint.x) {
+    func update(state:CircleState) -> State {
+        
+        let width = self.frame.width
+        let height = self.frame.height
+        
+        //            let dradius = state.3 + CGFloat(arc4random() % 3) - 1
+        //            var radius = state.2 + dradius/5
+        let dradius = state.radSpeed
+        let radius = state.radius
+        //            var radius = state.2 + CGFloat(arc4random() % 3) - 1
+        //            if radius < 1 {
+        //                radius = 1
+        //            } else if radius > 10 {
+        //                radius = 10
+        //            }
+        
+        var r = state.origin
+        var v = state.speed
+        
+        v = randumAddPoint(v)
+        
+        if touchmode {
+            v = touchEffect(state, touchPoint: touchPoint ?? center)
+        }
+        
+        r.x = r.x + v.x / 5
+        r.y = r.y + v.y / 5
+        if (r.y > height) {r.y = height;v.y = 0} else if (r.y <= 5)    {r.y = 5; v.y = 0}
+        if (r.x <= 5)     {r.x = 5; v.x = 0}     else if (r.x > width) {r.x = width;v.x = 0}
+        return CircleState(origin: r, speed: v, radius:radius, radSpeed:dradius)
+    }
+    
+    func update(list:[State]) -> [State] {
+        return list.map(update)
+    }
+    
+    func touchEffect(state:State, touchPoint:CGPoint) -> CGPoint {
+        var v : CGPoint = state.speed
+        if (state.origin.x > touchPoint.x) {
             v.x = v.x - 1
-        } else if (state.0.x < touchPoint.x) {
+        } else if (state.origin.x < touchPoint.x) {
             v.x = v.x + 1
         }
-        if (state.0.y > touchPoint.y) {
+        if (state.origin.y > touchPoint.y) {
             v.y = v.y - 1
-        } else if (state.0.y < touchPoint.y) {
+        } else if (state.origin.y < touchPoint.y) {
             v.y = v.y + 1
         }
         
@@ -218,18 +347,10 @@ class AnimalView: UIView {
         context?.setStrokeColor(1, green: 1, blue: 1, alpha: 1)
         context?.setLineCap(CGLineCap.Round)
         
-        for p in list! { drawLine(p.origin, direction: p.direction, length: p.length)}
+        for p in list! { p.draw(self)}
     }
 
-    func start() {
-        
-        list  = initialState(100,
-                             point:center,
-                             direction:CGPoint(x:0,y:1),
-                                length:10,
-                                life:5)
-        NSTimer.scheduledTimerWithTimeInterval(0.016, target: self, selector: #selector(AnimalView.timeFired(_:)), userInfo: nil, repeats: true)
-    }
+
     
     func timeFired(timer:NSTimer) {
         
@@ -238,166 +359,28 @@ class AnimalView: UIView {
         self.setNeedsDisplay()
     }
     
-    func initialState(n:NSInteger, point:CGPoint, direction:CGPoint, length:CGFloat, life:CGFloat)->[LineState]
+    func initialState(n:NSInteger, point:CGPoint?, direction:CGPoint, length:CGFloat, life:CGFloat)->[State]
     {
-        var l:[LineState] = []
+        var l:[State] = []
         for _ in 0 ... n {
-            let state:LineState = LineState(point,
-                                            dVec(x:0,y:0),
-                                            direction,
-                                            length,
-                                            life)
+            let state:LineState = LineState(origin:point ?? center,
+                                            speed:dVec(x:0,y:0),
+                                            direction:direction,
+                                            length:length,
+                                            life:life)
             l.append(state)
         }
         return l
     }
-}
-
-
-
-class AnimalView2: UIView {
     
-    var touchmode:Bool = false
-    var touchPoint:CGPoint = CGPoint.zero
-    
-    //var context:CGContext?
-    var list:[State]? = []
-    
-    var context:CGContext? = nil
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        start()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-            start()
-    }
-
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        touchmode = true
-        if let touch:UITouch = touches.first {
-            let point = touch.location(self)
-            touchPoint = point
-        }
-    }
-    
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if let touch:UITouch = touches.first {
-            let point = touch.location(self)
-            touchPoint = point
-        }
-    }
-    
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        touchmode = false
-    }
-    
-    func drawCircle(rect:CGRect) {
-        
-        context?.strokeEllipse(rect)
-        context?.strokePath()
-    }
-    
-    func drawCircle(point:CGPoint, radius:CGFloat) {
-        
-        let r = CGRect(x: point.x - radius, y: point.y - radius, width: radius*2, height: radius * 2)
-        drawCircle(r)
-    }
-    
-    func drawCirclePoint(point:CGPoint, r:Radius) {
-        
-        drawCircle(point, radius: r)
-    }
-    
-    func update(list:[State]) -> [State] {
-
-        let width = self.frame.width
-        let height = self.frame.height
-        
-        return list.map { (state:State) -> State in
-            
-//            let dradius = state.3 + CGFloat(arc4random() % 3) - 1
-//            var radius = state.2 + dradius/5
-            let dradius = state.3
-            let radius = state.2
-//            var radius = state.2 + CGFloat(arc4random() % 3) - 1
-//            if radius < 1 {
-//                radius = 1
-//            } else if radius > 10 {
-//                radius = 10
-//            }
-            
-            var r = state.0
-            var v = state.1
-
-            v = randumAddPoint(v)
-            
-            if touchmode {
-                v = touchEffect(state)
-            }
-            
-            r.x = r.x + v.x / 5
-            r.y = r.y + v.y / 5
-            if (r.y > height) {r.y = height;v.y = 0} else if (r.y <= 5)    {r.y = 5; v.y = 0}
-            if (r.x <= 5)     {r.x = 5; v.x = 0}     else if (r.x > width) {r.x = width;v.x = 0}
-            return (r,v, radius, dradius)
-        }
-    }
-    
-    func touchEffect(state:State) -> CGPoint {
-        var v : CGPoint = state.1
-        if (state.0.x > touchPoint.x) {
-            v.x = v.x - 1
-        } else if (state.0.x < touchPoint.x) {
-            v.x = v.x + 1
-        }
-        if (state.0.y > touchPoint.y) {
-            v.y = v.y - 1
-        } else if (state.0.y < touchPoint.y) {
-            v.y = v.y + 1
-        }
-        
-        return v
-    }
-    
-    override func drawRect(rect: CGRect) {
-        
-        context = UIGraphicsGetCurrentContext()
-        context?.clear(rect)
-        context?.setLineWidth(3)
-        context?.setStrokeColor(0, green: 0, blue: 1, alpha: 1)
-        context?.setLineCap(CGLineCap.Round)
-        
-        for p in list! { drawCirclePoint(p.0, r:p.2) }
-    }
-    
-    func start() {
-        
-        self.list  = initialState(100,
-                                  point:self.center,
-                                  radius: 1)
-        NSTimer.scheduledTimerWithTimeInterval(0.016, target: self, selector: #selector(AnimalView.timeFired(_:)), userInfo: nil, repeats: true)
-    }
-    
-    func timeFired(timer:NSTimer) {
-        
-        self.list =  update(self.list!)
-        self.setNeedsLayout()
-        self.setNeedsDisplay()
-    }
-    
-    func initialState(n:NSInteger, point:CGPoint, radius:CGFloat)->[State]
+    func initialState(n:NSInteger, point:CGPoint?, radius:CGFloat)->[State]
     {
         var l:[State] = []
         for _ in 0 ... n {
-            let state:State = State(point,
-                                    dVec(x:0,y:0),
-                                    10,
-                                    0)
+            let state:CircleState = CircleState(origin: point ?? center,
+                                                speed:dVec(x:0,y:0),
+                                                radius:10,
+                                                radSpeed:0)
             l.append(state)
         }
         return l
